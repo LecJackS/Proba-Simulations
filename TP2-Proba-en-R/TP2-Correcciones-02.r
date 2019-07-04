3bef6c6abc06866b64aed03d1dc5388fbe915f13
Unknown <jacksparrow.88@gmail.com>
2019-07-04 18:31:58 -0300

    Agrego .r y .pdf

---
+++ TP2-Proba-en-R/TP2-Correcciones-02.r
@@ -0,0 +1,489 @@
+
+# Función estimadora de primeros momentos
+bmom1 = function(muestra){
+    return(2*mean(muestra))
+}
+
+
+
+# Función estimadora de segundos momentos
+bmom2 = function(muestra){
+    n <- length(muestra)
+    return(sqrt(3 * mean(muestra^2)))
+}
+
+# Estimador de maxima verosimilitud
+bmv = function(muestra){
+    return(max(muestra))
+}
+
+bmed = function(muestra){
+    return(2*median(muestra))
+}
+
+b <- 1
+n <- 15
+muestra <- runif(n, min=0, max=b)
+
+bmom1(muestra)
+bmom2(muestra)
+bmv(muestra)
+bmed(muestra)
+
+# Calculo errores
+error_momento_1 <- b - bmom1(muestra)
+error_momento_2 <- b - bmom2(muestra)
+error_max_ver   <- b - bmv(muestra)
+error_mediana   <- b - bmed(muestra)
+# Agrupo datos para plot
+errores <- c(error_momento_1, error_momento_2, error_max_ver,error_mediana)
+nombres <- c('mom1','mom2','max.ver','mediana')
+# Imprimo y ploteo errores para una mejor comparación
+matrix(c(nombres, round(errores,5)), nrow=2, ncol=4, byrow=TRUE)
+# Plot
+#options(repr.plot.width=7, repr.plot.height=7)
+plot(errores, xlab="Estimadores", ylab="Error", xaxt='n')
+text(errores, as.character(round(errores,5)), cex=0.6, pos=1, col="red")
+axis(1, c(1,2,3,4), nombres) 
+
+experimento = function(){
+    # a)
+    b <- 1
+    n <- 15
+    muestra <- runif(n, min=0, max=b)
+    # b)
+    b_mom1 <- bmom1(muestra)
+    b_mom2 <- bmom2(muestra)
+    b_mv  <- bmv(muestra)
+    b_med <- bmed(muestra)
+    #devuelvo un vector de estimadores
+    c(b_mom1, b_mom2, b_mv, b_med)
+}
+
+# c)
+nrep <- 1000
+estimadores <- array(dim=c(nrep,4), dimnames=list(1:nrep, c("b_mom1", "b_mom2", "b_mv", "b_med")))
+for(i in 1:nrep){
+    estimadores[i,] <- array(experimento())
+}
+
+# Estimaciones guardadas de cada experimento
+estimadores[2:4,]
+estimadores[997:1000,]
+
+# d)
+# aplico mean a cada columna (estimador) de mi data
+b_muestrales <- apply(estimadores, MARGIN=2, FUN=mean)
+print(b_muestrales)
+
+#sesgos <- medias_muestrales - b
+b <- 1
+sesgos <- b_muestrales - b
+
+print(sesgos)
+
+# e)
+varianzas_muestrales <- apply(estimadores, MARGIN=2, FUN=var)
+
+print(varianzas_muestrales)
+
+# f) Aproximación del Error Cuadratico Medio (ECM)
+ECM <- varianzas_muestrales + sesgos^2
+print(ECM)
+
+# Funciones simuladoras:
+# Devuelven sesgo y varianza aproximados
+# promediando 1000 experimentos
+# con Estimador de Maxima Verosimilitud
+simulacion_mv = function(b, n){
+    nE <- 1000
+    # Guardo todas las estimaciones
+    all_b_est <- vector(length=nE)
+    varianza <- vector(length=nE)
+    for (i in 1:nE){
+        muestra <- runif(n, min=0, max=b)
+        b_est <- bmv(muestra)
+        # Guardo b estimado para calcular Sesgo/Var luego
+        all_b_est[i] <- b_est
+    }
+    # Calculo Sesgo y Varianza usando todas las muestras
+    sesgo_est <- mean(all_b_est) - b
+    # Calculo varianza muestral, usando b estimados
+    varianza_est <- var(all_b_est)
+    return(c(sesgo_est, varianza_est))
+}
+
+# con Estimador de 1er Momento
+simulacion_mom = function(b, n){
+    nE <- 1000
+    all_b_est <- vector(length=nE)
+    varianza <- vector(length=nE)
+    for (i in 1:nE){
+        muestra <- runif(n, min=0, max=b)
+        b_est <- bmom1(muestra)
+        all_b_est[i] <- b_est
+    }
+    sesgo_est <- mean(all_b_est) - b
+    varianza_est <- var(all_b_est)
+    return(c(sesgo_est, varianza_est))
+}
+
+# Agrego también simulación de 2do momento
+simulacion_mom2 = function(b, n){
+    nE <- 1000
+    all_b_est <- vector(length=nE)
+    varianza <- vector(length=nE)
+    for (i in 1:nE){
+        muestra <- runif(n, min=0, max=b)
+        b_est <- bmom2(muestra)
+        all_b_est[i] <- b_est
+        }
+    sesgo_est <- mean(all_b_est) - b
+    varianza_est <- var(all_b_est)
+    return(c(sesgo_est, varianza_est))
+}
+
+# con Mediana de la muestra
+simulacion_med = function(b, n){
+    nE <- 1000
+    all_b_est <- vector(length=nE)
+    varianza <- vector(length=nE)
+    for (i in 1:nE){
+        muestra <- runif(n, min=0, max=b)
+        b_est <- bmed(muestra)
+        all_b_est[i] <- b_est
+        }
+    sesgo_est <- mean(all_b_est) - b
+    varianza_est <- var(all_b_est)
+    return(c(sesgo_est, varianza_est))
+}
+
+# Calculo sesgos, varianzas y ECM para 20 valores
+# distintos de b entre 0 y 2 (no inclusives)
+nB <- 20
+b_values <- seq(0.1, 1.9, by=1.8/(nB-1))
+# nB filas, 3 columnas: (bias, var, ECM)
+results_mv   <- matrix(nrow=nB, ncol=3)
+results_mom  <- matrix(nrow=nB, ncol=3)
+results_mom2 <- matrix(nrow=nB, ncol=3)
+results_med  <- matrix(nrow=nB, ncol=3)
+
+for (i in 1:nB){
+    b <- b_values[i]
+    results_mv[i,1:2]   <- simulacion_mv(b, 15)
+    results_mom[i,1:2]  <- simulacion_mom(b, 15)
+    results_mom2[i,1:2] <- simulacion_mom2(b, 15)
+    results_med[i,1:2]  <- simulacion_med(b, 15)
+    # ECM = Var + Sesgo^2
+    results_mv[i,3]   <- results_mv[i,1]^2   + results_mv[i,2]
+    results_mom[i,3]  <- results_mom[i,1]^2  + results_mom[i,2]
+    results_mom2[i,3] <- results_mom2[i,1]^2 + results_mom2[i,2]
+    results_med[i,3]  <- results_med[i,1]^2  + results_med[i,2]
+}
+
+par(mfrow=c(2,2))
+# Sesgos
+plot(b_values, results_mv[,1], ylim=c(-0.2,0.1), xlim=c(0,2),
+     col="blue", main="Sesgo", xlab="valores para b", ylab="Sesgo", type="b")
+points(b_values, results_mom[,1], col="red", type="b")
+points(b_values, results_mom2[,1], col="orange", type="b")
+points(b_values, results_med[,1], col="black", type="b")
+grid()
+transpa_color <- rgb(0, 0, 0, max = 255, alpha = 0, names = "transparent")
+legend("topleft", bg=transpa_color,legend=c("MaxVer", "EMom1", "EMom2", "Mediana"),
+       col=c("blue", "red", "orange", "black"), lty=1, cex=0.8,
+       box.lty=1)
+# Varianzas
+plot(b_values, results_mv[,2], ylim=c(0,0.3), xlim=c(0,2),
+     col="blue", main="Varianza", xlab="valores para b", ylab="Varianza", type="b")
+points(b_values, results_mom[,2], col="red", type="b")
+points(b_values, results_mom2[,2], col="orange", type="b")
+points(b_values, results_med[,2], col="black", type="b")
+grid()
+transpa_color <- rgb(0, 0, 0, max = 255, alpha = 0, names = "transparent")
+legend("topleft", bg=transpa_color,legend=c("MaxVer", "EMom1", "EMom2", "Mediana"),
+       col=c("blue", "red", "orange", "black"), lty=1, cex=0.8,
+       box.lty=1)
+# ECM = Var + Sesgo^2
+plot(b_values, results_mv[,3], col="blue", main="ECM", xlab="valores para b", ylab="ECM", type="b", ylim=c(0.0,0.1))
+points(b_values, results_mom[,3], col="red", type="b")
+points(b_values, results_mom2[,3], col="orange", type="b")
+points(b_values, results_med[,3], col="black", type="b")
+grid()
+transpa_color <- rgb(0, 0, 0, max = 255, alpha = 0, names = "transparent")
+legend("topleft", bg=transpa_color,legend=c("MaxVer", "EMom1", "EMom2", "Mediana"),
+       col=c("blue", "red", "orange", "black"), lty=1, cex=0.8,
+       box.lty=1)
+
+# Calculo sesgos, varianzas para distintos valores de n
+n_values <- c(15, 30, 60, 120, 240)
+nN <- length(n_values)
+# nN filas, 4 columnas: (n, Sesgo, Var, ECM)
+results_mv   <- matrix(nrow=nN, ncol=4)
+results_mom  <- matrix(nrow=nN, ncol=4)
+results_mom2 <- matrix(nrow=nN, ncol=4)
+results_med  <- matrix(nrow=nN, ncol=4)
+
+for (i in 1:nN){
+    n <- n_values[i]
+    # Guardo n en [1]
+    results_mv[i,1]   <- n
+    results_mom[i,1]  <- n
+    results_mom2[i,1] <- n
+    results_med[i,1]  <- n
+    # Realizo 1000 simulaciones y guardo
+    # Sesgos[2] y Varianzas[3] para graficarlos
+    results_mv[i,2:3]   <- simulacion_mv(1, n)
+    results_mom[i,2:3]  <- simulacion_mom(1, n)
+    results_mom2[i,2:3] <- simulacion_mom2(1, n)
+    results_med[i,2:3]  <- simulacion_med(1, n)
+    # ECM[4] = Sesgo^2 + Var
+    results_mv[i,4]   <- results_mv[i,2]^2   + results_mv[i,3]
+    results_mom[i,4]  <- results_mom[i,2]^2  + results_mom[i,3]
+    results_mom2[i,4] <- results_mom2[i,2]^2 + results_mom2[i,3]
+    results_med[i,4]  <- results_med[i,2]^2  + results_med[i,3]
+}
+
+results_mv
+# n   Sesgo    Varianza   ECM
+
+par(mfrow=c(2,2))
+# Sesgos
+plot(results_mv[,c(1,2)], ylim=c(-0.10,0.05), xlim=c(0,250),
+     col="blue", main="Sesgo", xlab="tamaño de muestra n", ylab="Sesgo", type="b")
+points(results_mom[,c(1,2)], col="red", type="b")
+points(results_mom2[,c(1,2)], col="orange", type="b")
+points(results_med[,c(1,2)], col="black", type="b")
+grid()
+transpa_color <- rgb(0, 0, 0, max = 255, alpha = 0, names = "transparent")
+legend("bottomright", bg=transpa_color,legend=c("MaxVer", "EMom1", "EMom2", "Mediana"),
+       col=c("blue", "red", "orange", "black"), lty=1, cex=0.8,
+       box.lty=1)
+# Varianzas
+plot(results_mv[,c(1,3)], ylim=c(0.0,0.08), xlim=c(0,250),
+     col="blue", main="Varianza", xlab="tamaño de muestra n", ylab="Varianza", type="b")
+points(results_mom[,c(1,3)], col="red", type="b")
+points(results_mom2[,c(1,3)], col="orange", type="b")
+points(results_med[,c(1,3)], col="black", type="b")
+grid()
+transpa_color <- rgb(0, 0, 0, max = 255, alpha = 0, names = "transparent")
+legend("topright", bg=transpa_color,legend=c("MaxVer", "EMom1", "EMom2", "Mediana"),
+       col=c("blue", "red", "orange", "black"), lty=1, cex=0.8,
+       box.lty=1)
+# ECM 
+plot(results_mv[,c(1,4)], col="blue", main="ECM", xlab="tamaño de muestra n", ylab="ECM", type="b", ylim=c(0.0,0.08), xlim=c(0,250))
+points(results_mom[,c(1,4)], col="red", type="b")
+points(results_mom2[,c(1,4)], col="orange", type="b")
+points(results_med[,c(1,4)], col="black", type="b")
+grid()
+transpa_color <- rgb(0, 0, 0, max = 255, alpha = 0, names = "transparent")
+legend("topright", bg=transpa_color, legend=c("MaxVer", "EMom1", "EMom2", "Mediana"),
+       col=c("blue", "red", "orange", "black"), lty=1, cex=0.8,
+       box.lty=1)
+
+# Zoom en mayor n alcanzado
+plot(results_mv, ylim=c(-0.02, 0.02), xlim=c(220,245),
+     col="blue", main="ECM", xlab="tamaño de muestra n", ylab="ECM", type="b")
+points(results_mom, col="red", type="b")
+points(results_mom2, col="orange", type="b")
+points(results_med, col="black", type="b")
+grid()
+
+X <- c(0.917, 0.247, 0.384, 0.530, 0.798,
+       0.912, 0.096, 0.684, 0.394, 20.1,
+       0.769, 0.137, 0.352, 0.332, 0.670)
+
+b_mv <- bmv(X)
+b_mom <- bmom1(X)
+b_mom2 <- bmom2(X)
+b_med <- bmed(X)
+
+b_mv
+b_mom
+b_mom2
+b_med
+
+boxplot(X)
+
+X_fixed <- c(0.917, 0.247, 0.384, 0.530, 0.798,
+       0.912, 0.096, 0.684, 0.394,
+       0.769, 0.137, 0.352, 0.332, 0.670)
+
+b_mv <- bmv(X_fixed)
+b_mom <- bmom1(X_fixed)
+b_mom2 <- bmom2(X_fixed)
+b_med <- bmed(X_fixed)
+
+b_mv
+b_mom
+b_mom2
+b_med
+
+boxplot(X_fixed)
+
+n <- 15
+b <- 1
+X <- runif(n, 0, b)
+
+# Minicódigo a implementar en funciones simuladoras
+X_cont <- X
+# Contamino cada elemento con proba 0.005
+for(i in 1:n){
+    pC <- 0.005 # 1/200
+    if(runif(1) < pC){
+        
+        X_cont[i] <- X_cont[i] * 100
+    }
+}
+
+# De manera más eficiente (y bonita :)
+pC <- 1/200
+mask <- runif(n)
+X_cont[mask<pC] <- X_cont[mask<pC] * 100
+
+# Funciones simuladoras CON CONTAMINACIÓN:
+# Devuelven sesgo y varianza aproximados
+# promediando 1000 experimentos
+# con Estimador de Maxima Verosimilitud
+simulacion_mv_cont = function(b, n){
+    nE <- 1000
+    # Guardo todas las estimaciones
+    all_b_est <- vector(length=nE)
+    varianza <- vector(length=nE)
+    for (i in 1:nE){
+        muestra <- runif(n, min=0, max=b)
+        # --[Contaminación con proba pC]--
+        pC <- 1/200
+        mask <- runif(n)
+        muestra[mask<pC] <- muestra[mask<pC] * 100
+        # --[Fin contaminación]--
+        b_est <- bmv(muestra)
+        # Guardo b estimado para calcular Sesgo luego
+        all_b_est[i] <- b_est
+    }
+    # Calculo Sesgo y Varianza usando todas las muestras
+    sesgo_est <- mean(all_b_est) - b
+    varianza_est <- var(all_b_est)
+    return(c(sesgo_est, varianza_est))
+}
+
+# con Estimador de 1er Momento
+simulacion_mom_cont = function(b, n){
+    nE <- 1000
+    all_b_est <- vector(length=nE)
+    varianza <- vector(length=nE)
+    for (i in 1:nE){
+        muestra <- runif(n, min=0, max=b)
+        # --[Contaminación con proba pC]--
+        pC <- 1/200
+        mask <- runif(n)
+        muestra[mask<pC] <- muestra[mask<pC] * 100
+        # --[Fin contaminación]--
+        b_est <- bmom1(muestra)
+        all_b_est[i] <- b_est
+    }
+    sesgo_est <- mean(all_b_est) - b
+    varianza_est <- var(all_b_est)
+    return(c(sesgo_est, varianza_est))
+}
+
+# Agrego también simulación de 2do momento
+simulacion_mom2_cont = function(b, n){
+    nE <- 1000
+    all_b_est <- vector(length=nE)
+    varianza <- vector(length=nE)
+    for (i in 1:nE){
+        muestra <- runif(n, min=0, max=b)
+        # --[Contaminación con proba pC]--
+        pC <- 1/200
+        mask <- runif(n)
+        muestra[mask<pC] <- muestra[mask<pC] * 100
+        # --[Fin contaminación]--
+        b_est <- bmom2(muestra)
+        all_b_est[i] <- b_est
+        }
+    sesgo_est <- mean(all_b_est) - b
+    varianza_est <- var(all_b_est)
+    return(c(sesgo_est, varianza_est))
+}
+
+# con Mediana de la muestra
+simulacion_med_cont = function(b, n){
+    nE <- 1000
+    all_b_est <- vector(length=nE)
+    varianza <- vector(length=nE)
+    for (i in 1:nE){
+        muestra <- runif(n, min=0, max=b)
+        # --[Contaminación con proba pC]--
+        pC <- 1/200
+        mask <- runif(n)
+        muestra[mask<pC] <- muestra[mask<pC] * 100
+        # --[Fin contaminación]--
+        b_est <- bmed(muestra)
+        all_b_est[i] <- b_est
+        }
+    sesgo_est <- mean(all_b_est) - b
+    varianza_est <- var(all_b_est)
+    return(c(sesgo_est, varianza_est))
+}
+
+# Calculo sesgos, varianzas y ECM para 20 valores
+# distintos de b entre 0 y 2 (no inclusives)
+nB <- 20
+b_values <- seq(0.1, 1.9, by=1.8/(nB-1))
+# nB filas, 3 columnas: (bias, var, ECM)
+results_mv   <- matrix(nrow=nB, ncol=3)
+results_mom  <- matrix(nrow=nB, ncol=3)
+results_mom2 <- matrix(nrow=nB, ncol=3)
+results_med  <- matrix(nrow=nB, ncol=3)
+
+for (i in 1:nB){
+    b <- b_values[i]
+    # Guardo Sesgo y Varianza para cada b
+    results_mv[i,1:2]   <- simulacion_mv_cont(b, 15)
+    results_mom[i,1:2]  <- simulacion_mom_cont(b, 15)
+    results_mom2[i,1:2] <- simulacion_mom2_cont(b, 15)
+    results_med[i,1:2]  <- simulacion_med_cont(b, 15)
+    # Calculo ECM = Sesgo^2 + Var
+    results_mv[i,3] <- results_mv[i,1]^2+results_mv[i,2]
+    results_mom[i,3] <- results_mom[i,1]^2+results_mom[i,2]
+    results_mom2[i,3] <- results_mom2[i,1]^2+results_mom2[i,2]
+    results_med[i,3] <- results_med[i,1]^2+results_med[i,2]
+}
+
+par(mfrow=c(2,2))
+# Plot para Sesgos
+ylim <- c(-0.1, max(results_mv[,1]))
+plot(b_values, results_mv[,1], xlim=c(0,2), ylim=ylim, 
+     col="blue", main="Sesgo", xlab="valores para b", ylab="Sesgo", type="b")
+points(b_values, results_mom[,1], col="red", type="b")
+points(b_values, results_mom2[,1], col="orange", type="b")
+points(b_values, results_med[,1], col="black", type="b")
+grid()
+transpa_color <- rgb(0, 0, 0, max = 255, alpha = 0, names = "transparent")
+legend("topleft", bg=transpa_color,legend=c("MaxVer", "EMom1", "EMom2", "Mediana"),
+       col=c("blue", "red", "orange", "black"), lty=1, cex=0.8,
+       box.lty=1)
+# Plot para Varianzas
+plot(b_values, results_mv[,2], xlim=c(0,2),
+     col="blue", main="Varianza", xlab="valores para b", ylab="Varianza", type="b")
+points(b_values, results_mom[,2], col="red", type="b")
+points(b_values, results_mom2[,2], col="orange", type="b")
+points(b_values, results_med[,2], col="black", type="b")
+grid()
+transpa_color <- rgb(0, 0, 0, max = 255, alpha = 0, names = "transparent")
+legend("topleft", bg=transpa_color,legend=c("MaxVer", "EMom1", "EMom2", "Mediana"),
+       col=c("blue", "red", "orange", "black"), lty=1, cex=0.8,
+       box.lty=1)
+# Plot para ECM
+plot(b_values, results_mv[,3], col="blue", main="ECM", xlab="valores para b", ylab="ECM", type="b")
+points(b_values, results_mom[,3], col="red", type="b")
+points(b_values, results_mom2[,3], col="orange", type="b")
+points(b_values, results_med[,3], col="black", type="b")
+grid()
+transpa_color <- rgb(0, 0, 0, max = 255, alpha = 0, names = "transparent")
+legend("topleft", bg=transpa_color,legend=c("MaxVer", "EMom1", "EMom2", "Mediana"),
+       col=c("blue", "red", "orange", "black"), lty=1, cex=0.8,
+       box.lty=1)
+
+
